5ab1a4862e608a824701fa35b324c79c5fa35686
Dan Sabin <dan.sabin@gmail.com>
2014-10-25 18:22:32 +0000

    cleaning up hard coded answers

--- api_client.js
+++ api_client.js
@@ -1,72 +1,56 @@
 var SuperAgent = require('superagent'),
-  config = require("./config.js");
+    config = require("./config.js");
 
 var api_url = config.get('some_api').url;
-
 var authentication_64_encode = new Buffer(config.get('some_api').username + ':' + config.get('some_api').password).toString('base64')
 
-function get(url, callback, error_callback) {
+function get_api(url, callback, error_callback) {
   SuperAgent
     .get(api_url + url)
     .set('Accept', 'application/json')
     .set('Content-Type', 'application/json')
     .set('Authorization', 'Basic ' + authentication_64_encode)
     .end(function (error, response) {
-      if (response && response.ok) {
-        console.log('RESPONSE: ',response.text);
-        callback(response.text)
+      if (response) {
+        console.log('RESPONSE: ',response);
+        callback(response)
       } else {
-        if (response && response.text) {
-          console.log('API ERROR: ' + response.text);
-          error_callback(error);
-        } else {
-          console.log('UNKNOWN ERROR');
-          error_callback(error);
-        }
+        console.log('UNKNOWN ERROR');
+        error_callback(error);
       }
     });
 }
 
-function put(url, callback, error_callback) {
+function put_api(url, callback, error_callback) {
   SuperAgent
     .get(api_url + url)
     .set('Accept', 'application/json')
     .set('Content-Type', 'application/json')
     .set('Authorization', 'Basic ' + authentication_64_encode)
     .end(function (error, response) {
-      if (response && response.ok) {
-        console.log('RESPONSE: ',reponse.text);
-        callback(JSON.stringify(reponse.text))
+      if (response) {
+        console.log('RESPONSE: ',response);
+        callback(response)
       } else {
-        if (response && response.text) {
-          console.log('API ERROR: ' + response.text);
-          error_callback(error);
-        } else {
-          console.log('UNKNOWN ERROR');
-          error_callback(error);
-        }
+        console.log('UNKNOWN ERROR');
+        error_callback(error);
       }
     });
 }
 
-function post(url, callback, error_callback) {
+function post_api(url, callback, error_callback) {
   SuperAgent
     .get(api_url + url)
     .set('Accept', 'application/json')
     .set('Content-Type', 'application/json')
     .set('Authorization', 'Basic ' + authentication_64_encode)
     .end(function (error, response) {
-      if (response && response.ok) {
-        console.log('RESPONSE: ',reponse.text);
-        callback(JSON.stringify(reponse.text))
+      if (response) {
+        console.log('RESPONSE: ',response);
+        callback(response)
       } else {
-        if (response && response.text) {
-          console.log('API ERROR: ' + response.text);
-          error_callback(error);
-        } else {
-          console.log('UNKNOWN ERROR');
-          error_callback(error);
-        }
+        console.log('UNKNOWN ERROR');
+        error_callback(error);
       }
     });
 }
@@ -78,24 +62,19 @@ function delete_api(url, callback, error_callback) {
     .set('Content-Type', 'application/json')
     .set('Authorization', 'Basic ' + authentication_64_encode)
     .end(function (error, response) {
-      if (response && response.ok) {
-        console.log('RESPONSE: ',reponse.text);
-        callback(JSON.stringify(reponse.text))
+      if (response) {
+        console.log('RESPONSE: ',response);
+        callback(response)
       } else {
-        if (response && response.text) {
-          console.log('API ERROR: ' + response.text);
-          error_callback(error);
-        } else {
-          console.log('UNKNOWN ERROR');
-          error_callback(error);
-        }
+        console.log('UNKNOWN ERROR');
+        error_callback(error);
       }
     });
 }
 
 module.exports = {
-  get_api: get,
-  put_api: put,
-  post_api: post,
+  get_api: get_api,
+  put_api: put_api,
+  post_api: post_api,
   delete_api: delete_api
 };
--- server.js
+++ server.js
@@ -7,16 +7,19 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 http.createServer(function (request, response) {
   var uri = url.parse(request.url).pathname;
 
+  var headers = {
+    'Content-Type': 'application/json'
+  }
+  
+  // Check header for X-Request-For so we know what API to call, for now we call only one API
   api_client.get_api(uri,
-    function (body) {
-      //success case
-      response.writeHead(200, {'Content-Type': 'application/json'});
-      response.write(body, "utf8");
+    function (response) {
+      response.writeHead(response.code, headers);
+      response.write(response.text, "utf8");
       response.end();
     },
     function (details) {
-      //error case
-      response.writeHead(404);
+      response.writeHead(500);
       response.write(JSON.stringify(details), "utf8");
       response.end();
     }
