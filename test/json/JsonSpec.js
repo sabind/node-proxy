44e097810ea11f063464d9ad155e1c2a435b46d7
Dan Sabin <dsabin@beatsmusic.com>
2014-11-01 09:45:01 -0700

    added servers to tests

---
+++ test/json/JsonSpec.js
@@ -0,0 +1,40 @@
+var assert = require("assert"),
+  expect = require('chai').expect;
+
+
+describe('JSON Proxy', function(){
+  var server = require('../fake_json_api.js');
+
+  beforeEach(function (done) {
+    done();
+  });
+
+  afterEach(function (done) {
+    done();
+  });
+
+  describe('GET', function(){
+    it ('it should be able to execute a GET request', function (done) {
+      done();
+    });
+  });
+
+  describe('PUT', function(){
+    it ('it should be able to execute a PUT request', function (done) {
+      done();
+    });
+  });
+
+  describe('POST', function(){
+    it ('it should be able to execute a POST request', function (done) {
+      done();
+    });
+  });
+
+  describe('DELETE', function(){
+    it ('it should be able to execute a DELETE request', function (done) {
+      done();
+    });
+  });
+});
+
